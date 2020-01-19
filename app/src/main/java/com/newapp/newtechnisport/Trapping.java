b51823fc30c41e3c40fe7767ae1e4b5a25503140
ZubKharoti <zubairkharoti@gmail.com>
2020-01-19 05:15:10 -0800

    NewChanges

---
+++ app/src/main/java/com/newapp/newtechnisport/Trapping.java
@@ -0,0 +1,14 @@
+package com.newapp.newtechnisport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Trapping extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trapping);
+    }
+}
